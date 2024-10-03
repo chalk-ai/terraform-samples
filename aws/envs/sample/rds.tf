e6d3f57bf994f8fc81dbc6b2634d7f9f342d8b34
Ari Bennett <aridbennett@gmail.com>
2024-10-03 16:47:54 -0700

    updated rds

--- aws/envs/sample/rds.tf
+++ aws/envs/sample/rds.tf
@@ -1,1 +1,10 @@
-# Make sure PGCron is intalled
+module "rds" {
+  source                       = "../../modules/rds"
+  instance_name                = "${var.organization_name}-metadata-rds"
+  alert_topic_arn              = ""
+  vpc_id                       = module.vpc.id
+  subnet_ids                   = module.vpc.subnets
+  instance_class               = "db.t3.medium"
+  allocated_storage            = 50
+  performance_insights_enabled = true
+}
